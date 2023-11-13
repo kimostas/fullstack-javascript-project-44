f3ed73d4025209342bb62d1a64541501431ce986
Stanislav <stlove.kim@gmail.com>
2023-11-13 17:11:23 +0900

    fixed eslint error

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,4 +2,3 @@
 import evenGame from '../src/games/even.js';
 
 evenGame();
-
--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ export const getRandom = (max) => Math.round(Math.random() * max);
 export const getRandomRange = (min, max) => Math.round(Math.random() * (max - min) + min);
 
 export const basicOfGames = (purposeOfGame, task) => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log(purposeOfGame);
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log(purposeOfGame);
 
-  for (let i = 0; i < 3; i += 1) {
-    const questionAndResult = task();
+    for (let i = 0; i < 3; i += 1) {
+        const questionAndResult = task();
 
-    console.log(`Question: ${questionAndResult[0]}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const rightAnswer = questionAndResult[1];
-    if (userAnswer === rightAnswer) {
-      console.log('Correct!');
-    } else {
-      return console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}". \nLet's try again, ${name}!`);
+        console.log(`Question: ${questionAndResult[0]}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+        const rightAnswer = questionAndResult[1];
+        if (userAnswer === rightAnswer) {
+            console.log('Correct!');
+        } else {
+            return console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}". \nLet's try again, ${name}!`);
+        }
     }
-  }
-  return console.log(`Congratulations, ${name}!`);
+    return console.log(`Congratulations, ${name}!`);
 };
